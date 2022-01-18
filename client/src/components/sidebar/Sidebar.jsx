6d666d89067c11585843f4d1e0e4160bf7255737
NULLPointer25 <88246537+NULLPointer25@users.noreply.github.com>
2022-01-19 00:42:31 +0530

    Initial Commit

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        
+        <p>
+       Pratik Awadhesh Mishra
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW ME</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
